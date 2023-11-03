3204e182ae4b4e94ad230470b826b4bc82270457
annamaggg <annamagnusson99@gmail.com>
2023-11-03 17:35:59 +0000

    user homepage view created to list todos

--- app.rb
+++ app.rb
@@ -16,4 +16,41 @@ class Application < Sinatra::Base
   get '/' do
     return erb(:index)
   end
+
+  post '/login' do
+    account_repo = AccountRepository.new
+    all_usernames = account_repo.all_usernames
+    puts all_usernames
+
+    if all_usernames.include? params[:username]
+
+      account = account_repo.find_by_username(params[:username])
+      puts account
+      if params[:passkey] == account.passkey
+        session.clear
+        session[:user_id] = account.id
+        puts session[:user_id]
+        if session[:user_id] == nil
+          return "no session"
+          puts "no session"
+        else
+          puts 'error with redirecting'
+          redirect '/homepage'
+        end
+      else
+        puts "Password is incorrect, please try again"
+        erb(:index)
+      end
+    else 
+      puts "Username does not exist"
+      erb(:index)
+    end
+  end
+
+  get '/homepage' do
+    todo_repo = TodoRepository.new
+    @todos = todo_repo.todos_by_account_id(session[:user_id])
+    puts @todos
+    return erb(:homepage)
+  end
 end
