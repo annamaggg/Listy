487cd42ec42d7b033d12c47b7d58ac489a8c75a9
annamaggg <annamagnusson99@gmail.com>
2023-11-03 16:53:15 +0000

    acc repo spec test passing - returns all accounts

--- lib/account_repository.rb
+++ lib/account_repository.rb
@@ -2,7 +2,7 @@ require_relative 'account'
 require_relative 'database_connection'
 
 class AccountRepository
-  def all_accounts()
+  def all_accounts
     sql = "SELECT id, username, passkey FROM accounts"
     results = DatabaseConnection.exec_params(sql, [])
 
@@ -12,9 +12,40 @@ class AccountRepository
       account.id = data['id']
       account.username = data['username']
       account.passkey = data['passkey']
-      accounts << account
+      all_accounts << account
     end
 
     return all_accounts
+  end
+
+  def all_usernames 
+    sql = 'SELECT * FROM accounts;'
+    results = DatabaseConnection.exec_params(sql, [])
+
+    usernames = []
+    results.each do |item|
+      usernames << item['username']
+    end
+
+    return usernames
+  end
+
+  def create(account)
+    sql = 'INSERT INTO accounts (username, passkey) VALUES ($1, $2)'
+    result = DatabaseConnection.exec_params(sql, [account.username, account.passkey])
+    return result
+  end
+
+  def find_by_username(username)
+    sql = 'SELECT id, username, passkey FROM accounts WHERE username = $1;'
+      params = [username]
+      result = DatabaseConnection.exec_params(sql, params).first
+
+      account = Account.new
+      account.id = result['id'].to_i
+      account.username = result['username']
+      account.passkey = result['passkey']
+      
+      return account
   end
 end
---
+++ spec/account_repository_spec.rb
@@ -0,0 +1,20 @@
+require 'account_repository'
+require 'database_connection'
+
+def reset_tables 
+  seed_sql = File.read('spec/seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'listy-db' })
+  connection.exec(seed_sql)
+end
+
+describe AccountRepository do 
+  before(:each) do 
+    reset_tables
+  end
+
+  it "returns all accounts" do
+    repo = AccountRepository.new
+    expect(repo.all_accounts.length).to eq(1)
+    expect(repo.all_accounts.last.username).to eq('annamag')
+  end
+end
